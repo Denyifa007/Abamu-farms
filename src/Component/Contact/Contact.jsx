3529d4de48b1577401fcac2fdda80d069d84d0f4
Oyeindenyifa Diegbegha <121164225+Denyifa007@users.noreply.github.com>
2023-06-14 17:17:55 +0100

    commit

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Contact = () => {
   return (
-    <div name='contact' className=' w-full h-screen flex justify-center items-center te p-4'>
-        <form action="https://getform.io/f/d9ebf626-2052-4681-95c5-d39d0831a768" method='POST' className='flex flex-col max-w-[600px] text-black w-full'>
+    <div name='contact' className=' w-full h-screen flex justify-center items-center p-4'>
+        <form action="https://getform.io/f/d9ebf626-2052-4681-95c5-d39d0831a768" method='POST' className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-green-600 text-black-300'>Contact</p>
                 <p className='text-black-300 py-4'>//Submit the form below to send us an Email - <span className='text-red-700' style={{cursor:'pointer'}}>abamuenterprise@gmail.com</span> </p>
@@ -13,7 +14,8 @@ const Contact = () => {
             <input className='bg-[#82f278] p-2 my-4' type="number" placeholder='Phone no' name='phone no' />
             <input className='my-4 p-2 bg-[#82f278]' type="email" placeholder='Email' name='email' />
             <textarea className='bg-[#82f278]  p-2' name="message" rows="10" placeholder='Message'></textarea>
-            <button className='text-black group border-2 border-black hover:bg-green-600 hover:border-green-600 px-4 py-3 my-8 mx-auto flex items-center'>Submit</button>
+            <button className='text-black group border-2 rounded-md  bg-gradient-to-r from-green-300 to-green-600 hover:bg-green-600 hover:border-green-600  px-4 py-3 my-8 mx-auto flex items-center'> <span><HiArrowNarrowRight className='mr-3'/></span>  Submit</button>
+            
         </form>
     </div>
   )
